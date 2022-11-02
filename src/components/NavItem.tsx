2cd6aceae7e0bc7d041b04924bbbcb995b9b23a1
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-02 17:03:45 +0000

    changes

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -5,7 +5,7 @@ interface NavItemProps {
 function NavItem(props: NavItemProps): JSX.Element {
   return (
     <>
-      <a href="#">~{props.label.toUpperCase}</a><p>→</p>
+      <a href="#">~{props.label.toUpperCase()}</a><p>→</p>
       <span> | </span>
     </>
   );
